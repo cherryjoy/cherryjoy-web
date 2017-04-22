c7c8145bb24d3431a9f6c4bb366f849255eee158
smartlean <smartlean1@gmail.com>
2017-04-22 10:18:42 +0800

    modify

--- html/wp-content/themes/appointment/theme_setup_data.php
+++ html/wp-content/themes/appointment/theme_setup_data.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 function theme_setup_data()
   	{
 	return $appointment_options=array(
@@ -19,7 +19,7 @@ function theme_setup_data()
 	'twitter_media_enabled' => 0,
 	'linkedin_media_enabled' => 0,
 	'webrit_custom_css' => '',
-	
+
 	//Slider Default settings
 	'home_banner_enabled' => '',
 	'slider_radio' => 'demo',
@@ -27,11 +27,11 @@ function theme_setup_data()
 	'slider_options' => 'slide',
 	'slider_transition_delay' => 2000,
 	'featured_slider_post' => '',
-	
+
 	//Service section settings
 	'service_section_enabled' => '',
 	'service_title' => __('樱桃红中麻将','appointment'),
-	'service_description' => __('红中麻将是平江地区一种休闲娱乐的麻将，节奏快速，极易胡牌。。。','appointment'),
+	'service_description' => __('红中麻将是一种休闲娱乐的熟人麻将，节奏快速，极易胡牌。。。','appointment'),
 	'service_one_icon' => 'fa-mobile',
 	'service_one_title'=>__('拖至底部下载','appointment'),
 	'service_one_description' => __('','appointment'),
@@ -50,19 +50,19 @@ function theme_setup_data()
 	'service_six_icon' => 'fa-cog',
 	'service_six_title'=>__('严禁赌博','appointment'),
 	'service_six_description' => __('','appointment'),
-	
+
 	//Home callout section
 	'home_call_out_area_enabled' => '',
 	'home_call_out_title' => __('游戏','appointment'),
 	'home_call_out_description' =>  __('快来玩樱桃红中麻将，就等你下载','appointment'),
 	'callout_background' => '',
 	'home_call_out_btn1_text' =>__('安卓下载','appointment'),
-	'home_call_out_btn1_link' => 'http://fir.im/hzandroid',
+	'home_call_out_btn1_link' => 'http://hzmj.applinzi.com/cjhzmj_peixian.apk',
 	'home_call_out_btn1_link_target' => '',
 	'home_call_out_btn2_text' => __('IOS下载','appointment'),
-	'home_call_out_btn2_link' => 'http://fir.im/hzapple',
+	'home_call_out_btn2_link' => 'http://fir.im/hzapplepx',
 	'home_call_out_btn2_link_target' => '',
-	
+
 	//News Section
 	'home_blog_enabled' => '',
 	'home_meta_section_settings' => '',
@@ -70,7 +70,7 @@ function theme_setup_data()
 	'blog_description' => __('更多资讯请在本站的归档中阅读。','appointment'),
 	'blog_selected_category_id'=> 1,
 	'post_display_count' => '4',
-	
+
 	//Footer Copyright & footer social links
 	'footer_copyright_text' => __('Copyright © 樱桃互娱 2016','appointment'),
 	'footer_menu_bar_enabled' => '',
